1fd7bc837cecbf3b0894752ff037eeda04d83506
yyj0917 <yyj0917@yonsei.ac.kr>
2025-01-11 09:36:10 +0900

    Init : styles, package 일부 세팅

---
+++ Y5-front/src/styles/GlobalStyle.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+    ${reset}
+
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
+    a, dl, dt, dd, ol, ul, li, form, label, table{
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 16px;
+        vertical-align: baseline;
+    }
+    body{
+        line-height: 1;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        display: flex; 
+        flex-direction: column;
+        justify-content: center; 
+    }
+    ol, ul{
+        list-style: none;
+    }
+    button {
+        border: 0;
+        background: transparent;
+        cursor: pointer;
+    }
+
+    html {
+        font-size: 16px;
+    }
+`;
+
+export default GlobalStyles;
